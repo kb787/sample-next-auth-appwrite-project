9635066c645ca11644b46e07d62e4bc45c136fa5
Karan <karanbhanu788@gmail.com>
2024-02-21 16:03:29 +0530

    auth-frontend-components

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation" ;
 import axios from "axios"  ;
 import {toast} from "react-hot-toast" ;
 
-export default function Signup () {
+export default function Login () {
     const router = useRouter() ;
     const [user,setUser] = React.useState({
        userName:"",
@@ -43,18 +43,14 @@ export default function Signup () {
 
     return (
        <div className="flex items-center justify-center min-h-screen bg-slate-200"> 
-       <div className="flex flex-col justify-center align-middle bg-slate-400 border-2  border-black p-5 rounded-xl h-5/6 pt-6 pb-6 pr-15 pl-15 w-96">
-       <h5 className = "text-2xl text-center font-serif">Complete your registration</h5>
-        <input type='text' value={user.userName} placeholder="Enter your username" className="border-black rounded-xl mb-4 p-2 text-xl w-35 h-13 mt-3" onChange = {(e) => setUser(prevState => ({...prevState, userName: e.target.value}))}/>
+       <div className="flex flex-col justify-center align-middle bg-slate-400 border-2  border-black p-5 rounded-xl min-h-96 pt-2 pb-10 pr-15 pl-15 w-96">
+       <h5 className = "text-2xl text-center font-serif">Verfify your credentials</h5>
+        <input type='email' value={user.userEmail} placeholder="Enter your email address" className="border-black rounded-xl mb-4 p-2 text-xl w-35 h-13 mt-3" onChange = {(e) => setUser(prevState => ({...prevState, userEmail: e.target.value}))} />
         <br />
-        <input type='email' value={user.userEmail} placeholder="Enter your email address" className="border-black rounded-xl mb-4 p-2 text-xl w-35 h-13" onChange = {(e) => setUser(prevState => ({...prevState, userEmail: e.target.value}))} />
-        <br />
-        <input type='password' value={user.userPassword} placeholder="Enter your password" className="border-black rounded-xl p-2 text-xl w-35 h-13" onChange = {(e) => setUser(prevState => ({...prevState, userPassword: e.target.value}))} />
+        <input type='password' value={user.userPassword} placeholder="Enter your password" className="border-black rounded-xl mb-4 p-2 text-xl w-35 h-13" onChange = {(e) => setUser(prevState => ({...prevState, userPassword: e.target.value}))} />
         <br/>
-        <button className = 'bg-blue-500 rounded-md w-28 h-12 text-xl text-white flex justify-center items-center mx-24'>Register</button>
+        <button className = 'bg-blue-500 rounded-md w-28 h-12 text-xl text-white flex justify-center items-center mx-24'>Login</button>
     </div>
 </div>
     )
 }
-
-
